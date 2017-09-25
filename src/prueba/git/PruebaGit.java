17f594d3fab0ebbdc4bc1bb7787a0b0025123027
jdtorres1997 <juan.torres.canon@correounivalle.edu.co>
2017-09-25 10:01:37 -0500

    Modificando

--- src/prueba/git/PruebaGit.java
+++ src/prueba/git/PruebaGit.java
@@ -17,7 +17,6 @@ public class PruebaGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo");
-        System.out.println("WTF");
     }
     
 }
